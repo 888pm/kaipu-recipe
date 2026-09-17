fd8bc0c1a9639f356b575457660e1970c72ade3d
888pm <3528315660@qq.com>
2026-09-17 17:42:40 +0800

    add android project: app/src/main/java/com/kaipu/recipe/MainActivity.java

---
+++ app/src/main/java/com/kaipu/recipe/MainActivity.java
@@ -0,0 +1,54 @@
+package com.kaipu.recipe;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.content.Intent;
+import android.net.Uri;
+
+public class MainActivity extends Activity {
+    private WebView wv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        wv = new WebView(this);
+        WebSettings ws = wv.getSettings();
+        ws.setJavaScriptEnabled(true);
+        ws.setDomStorageEnabled(true);
+        ws.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        wv.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return handleExternal(url);
+            }
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest req) {
+                return handleExternal(req.getUrl().toString());
+            }
+            private boolean handleExternal(String url) {
+                if (url.startsWith("https://888pm.github.io/")) {
+                    return false; // 站内页面留在 App 内
+                }
+                // 小红书/抖音等外部链接，跳到系统浏览器或对应 App 打开
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(i);
+                return true;
+            }
+        });
+        setContentView(wv);
+        wv.loadUrl("https://888pm.github.io/kaipu-recipe/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (wv != null && wv.canGoBack()) {
+            wv.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
